3fab7f7cfff7df48535a9a3d4d2abed183f42ea4
Wemerson Walcley <wemerson.walcley@outlook.com>
2022-01-27 04:28:44 -0300

    Atualização da senha do banco h2 e dos imports em sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,9 @@
 INSERT INTO tb_departament (name) VALUES ('Gestão')
 INSERT INTO tb_departament (name) VALUES ('Informática')
 
-INSERT INTO tb_user (name, email, password, departament_id) VALUES ('Wallas', 'wallas123@outlook.com', 'maconhabraba666', 1)
-INSERT INTO tb_user (name, email, password, departament_id) VALUES ('Ricardo Stronda', 'RicardoGamer14@outlook.com', 'ricardo123', 2)
+INSERT INTO tb_user (name, email, password, departament_id) VALUES ('Wallas Souza', 'wallasSouza@outlook.com', 'coxinha123', 1)
+INSERT INTO tb_user (name, email, password, departament_id) VALUES ('Ricardo Florêncio', 'RicardoFlorencio@outlook.com', 'ricardo123', 2)
+INSERT INTO tb_user (name, email, password, departament_id) VALUES ('Hermes Neto', 'HermesNeto@gmail.com', 'neto98834', 2)
+INSERT INTO tb_user (name, email, password, departament_id) VALUES ('Paulo Freitas', 'PauloF@gmail.com', 'freitas83334', 2)
+INSERT INTO tb_user (name, email, password, departament_id) VALUES ('Ivison Vinicius', 'IvisonVini@hotmail.com', 'vinicius9321', 1)
+INSERT INTO tb_user (name, email, password, departament_id) VALUES ('Marcelo', 'Marcelo123@gmail.com', 'marcelo224', 1)
