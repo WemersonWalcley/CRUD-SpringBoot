17f6da3ffef3355358060c2608d53de796cb793b
Wemerson Walcley <wemerson.walcley@outlook.com>
2022-02-24 09:46:40 -0300

    Adição de validações para campos nulos e para E-mail

--- src/main/java/com/github/wemerson/CRUD/entities/User.java
+++ src/main/java/com/github/wemerson/CRUD/entities/User.java
@@ -1,23 +1,34 @@
 package com.github.wemerson.CRUD.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tb_user")
 @Getter
 @Setter
 @NoArgsConstructor
+@Validated
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @Email
     private String email;
+
+    @NotBlank
     private String password;
 
     @ManyToOne
